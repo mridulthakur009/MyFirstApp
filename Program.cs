62433e77514d0e06432f1409cf871a9999bbc342
Mridul Thakur <mridul.thakur.007@gmail.com>
2025-12-25 16:27:05 +0530

    HTTP response code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async(HttpContext context) =>
+{
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("Hello! ");  //WriteAsync method Writes the given text to the response body. UTF-8 encoding will be used.
+    await context.Response.WriteAsync("This is my first .NET Core Web Application");
+});
 
 app.Run();
